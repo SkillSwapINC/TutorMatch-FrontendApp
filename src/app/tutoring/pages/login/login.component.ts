11b744a2d2e2ce940655b4e1378040bcd8640c36
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-23 18:22:27 -0500

    feat(tutoring): add login page component.

---
+++ src/app/tutoring/pages/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCard, MatCardContent, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {LanguageSwitcherComponent} from "../../../public/components/language-switcher/language-switcher.component";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatToolbar,
+    MatButton,
+    MatCard,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatFormFieldModule, MatInputModule, MatSelectModule, MatIcon, MatIconButton, MatMenuTrigger, LanguageSwitcherComponent, MatMenu
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+
+    }
+  }
+
+  onLogin() {
+    this.router.navigate(['LogIn']).then();
+  }
+
+  onSignUp() {
+    this.router.navigate(['Register']).then();
+  }
+}
