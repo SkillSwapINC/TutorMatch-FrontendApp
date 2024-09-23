3f8792c9ca319b7819fd92459fe85ce350d4c123
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-23 18:51:42 -0500

    feat(main): edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {DashboardModule} from "./tutoring/components/dashboard/dashboard.module";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DashboardModule, FooterContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TutorMatch-FrontendApp';
+
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('en');
+  }
 }
