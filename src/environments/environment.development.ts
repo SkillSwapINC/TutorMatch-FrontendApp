46b1a85fde3fe2fb28785b4a192167e211669c65
RodrLH <u202212338@upc.edu.pe>
2024-09-23 16:27:07 -0500

    chore: configure environment variables support.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  // Server Base Path for Fake REST API
+  serverBasePath: 'http://localhost:3000/api/v1'
+  // Server Base Path for Spring Boot REST API
+  //serverBasePath: 'http://localhost:8080/api/v1'
+};
