0aad9627a604a71461c29309d45f67176b778574
RodrLH <u202212338@upc.edu.pe>
2024-09-23 19:05:36 -0500

    feat(in-app-navigation): setup in-app navigation rules.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
+import {DashboardComponent} from "./tutoring/components/dashboard/dashboard.component";
+import {RegisterComponent} from "./tutoring/pages/register/register.component";
+import {LoginComponent} from "./tutoring/pages/login/login.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:'Dashboard',                component:DashboardComponent},
+  {path:'LogIn',                  component:LoginComponent},
+  {path:'Register',                 component:RegisterComponent},
+  { path: '',                       redirectTo: 'Dashboard', pathMatch: 'full' },
+  { path: '**',                     component: PageNotFoundComponent }
+
+];
