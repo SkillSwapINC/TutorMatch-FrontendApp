3b757db0311a1af016ae0b51815684cd473e9713
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-12 14:06:51 -0500

    feat(in-app-navigation): setup settings navigation rule.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-foun
 import {DashboardComponent} from "./tutoring/components/dashboard/dashboard.component";
 import {RegisterComponent} from "./public/pages/register/register.component";
 import {LoginComponent} from "./public/pages/login/login.component";
+import {SettingsComponent} from "./public/pages/settings/settings.component";
 
 
 export const routes: Routes = [
   {path:'Dashboard',                component:DashboardComponent},
   {path:'LogIn',                  component:LoginComponent},
   {path:'Register',                 component:RegisterComponent},
+  { path: 'Settings',               component: SettingsComponent },
   { path: '',                       redirectTo: 'Dashboard', pathMatch: 'full' },
   { path: '**',                     component: PageNotFoundComponent }
 
