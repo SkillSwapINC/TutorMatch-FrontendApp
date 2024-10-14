c19d81126de80e7143863b7d02f3c16640c3765d
HamesEl <u202212684@upc.edu.pe>
2024-10-13 19:22:49 -0500

    feat(tutoring): add plans route and its path.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-foun
 import {DashboardComponent} from "./tutoring/components/dashboard/dashboard.component";
 import {RegisterComponent} from "./tutoring/pages/register/register.component";
 import {LoginComponent} from "./tutoring/pages/login/login.component";
+import {PlansComponent} from "./tutoring/components/plans/plans.component";
 
 
 export const routes: Routes = [
   {path:'Dashboard',                component:DashboardComponent},
   {path:'LogIn',                  component:LoginComponent},
   {path:'Register',                 component:RegisterComponent},
+  {path:'Plans',                 component:PlansComponent},
   { path: '',                       redirectTo: 'Dashboard', pathMatch: 'full' },
   { path: '**',                     component: PageNotFoundComponent }
 
