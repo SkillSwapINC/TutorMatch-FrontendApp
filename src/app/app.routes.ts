cc526c3bb01edf9fdc98aad7d25f78e2da770875
vrokerev <vmeneses782000@gmail.com>
2024-10-25 16:38:18 -0500

    feat: add new students components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import {LoginComponent} from "./public/pages/login/login.component";
 import {SettingsComponent} from "./public/pages/settings/settings.component";
 import {ProfileComponent} from "./public/pages/profile/profile.component";
 import {PlansComponent} from "./tutoring/components/plans/plans.component";
+import {CourseDetailComponent} from "./tutoring/components/dashboard/add-tutoring-dialog/course-detail/course-detail.component";
+import {CourseListComponent} from "./tutoring/components/dashboard/course-list/course-list.component";
 
 export const routes: Routes = [
   {path:'Dashboard',                component:DashboardComponent},
@@ -14,6 +16,8 @@ export const routes: Routes = [
   {path:'Plans',                    component:PlansComponent},
   { path: 'Settings',               component: SettingsComponent },
   {path:'Profile',                  component:ProfileComponent},
+  { path: 'courses/:cycle', component: CourseListComponent },
+  { path: 'course/:id', component: CourseDetailComponent },
   { path: '',                       redirectTo: 'LogIn', pathMatch: 'full' },
   { path: '**',                     component: PageNotFoundComponent }
 ];
--- src/app/tutoring/services/tutoring.service.ts
+++ src/app/tutoring/services/tutoring.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from '@angular/core';
-import {BaseService} from "../../shared/services/base.service";
-
+import { Injectable } from '@angular/core';
+import { BaseService } from "../../shared/services/base.service";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,31 @@ export class TutoringService extends BaseService<any> {
 
   constructor() {
     super();
+
     this.resourceEndPoint = '/tutorings';
+  }
+
+
+  getCourses(): Observable<any> {
+    this.resourceEndPoint = '/courses';
+    return this.http.get(this.resourcePath());
+  }
+
+  // Obtener tutoría por courseId (para el detalle del curso)
+  getTutoringByCourseId(courseId: number): Observable<any> {
+    this.resourceEndPoint = '/tutorings';
+    return this.http.get(`${this.resourcePath()}?courseId=${courseId}`);
+  }
+
+
+  getCoursesBySemester(cycle: number): Observable<any> {
+    this.resourceEndPoint = '/courses';
+    return this.http.get(`${this.resourcePath()}?cycle=${cycle}`);
+  }
+
+
+  getTutorById(tutorId: number): Observable<any> {
+    this.resourceEndPoint = '/tutors';
+    return this.http.get(`${this.resourcePath()}/${tutorId}`);
   }
 }
