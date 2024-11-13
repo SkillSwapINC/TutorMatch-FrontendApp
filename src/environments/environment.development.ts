63c63a25cec0403f0ea45a46cdada6101f6c6ed4
RodrLH <u202212338@upc.edu.pe>
2024-11-12 21:24:22 -0500

    chore: update serverBasePath

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,4 @@ export const environment = {
   // Server Base Path for Fake REST API
   serverBasePath: 'https://tutormatch-api.onrender.com'
   // Server Base Path for Spring Boot REST API
-  //serverBasePath: 'http://localhost:8080/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   serverBasePath: 'https://tutormatch-api.onrender.com'
+  //serverBasePath: 'http://localhost:8080/api/v1'
 };
